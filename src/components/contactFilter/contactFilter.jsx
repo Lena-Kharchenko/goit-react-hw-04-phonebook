4af736219446b4c2dcf11454910d7099f56431fb
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-06-13 13:56:48 +0300

    update

--- src/components/contactFilter/contactFilter.jsx
+++ src/components/contactFilter/contactFilter.jsx
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types';
 
-const ContactFilter = ({ onChange }) => {
+export default function ContactFilter({ onChange }) {
   return (
     <>
       <p>Please enter the name </p>
       <input type="text" name="filter" onChange={onChange}></input>
     </>
   );
-};
-
-export default ContactFilter;
+}
 
 ContactFilter.propTypes = {
   onChange: PropTypes.func.isRequired,
